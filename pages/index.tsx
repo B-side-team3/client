9c05801752b325313d3a2944c4612d4565049f45
jhpark <hareguu89@gmail.com>
2022-12-04 22:16:25 +0900

    Kakao login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,29 @@
-const Home = () => {
-  return <div>main page</div>;
+import { useEffect } from "react";
+import type { NextPage } from "next";
+import { signIn, useSession, signOut } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.name} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn("kakao")}>Sign in</button>
+    </>
+  );
 };
+
 export default Home;
